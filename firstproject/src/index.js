c81ba87bd1faa102b1a140e0a9e1cc3f43d0fe49
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-08-29 11:26:22 +0300

    Up

--- firstproject/src/index.js
+++ firstproject/src/index.js
@@ -17,43 +17,63 @@ import ReactDOM from "react-dom/client";
 //   );
 // }
 
+// const Greeting = () => {
+//   return (
+//     <React.Fragment>
+//       {React.createElement("h2", {}, "Hello from react")}
+//       {React.createElement(
+//         "ul",
+//         {}, // ul elementinin özellikleri (props) burada boş
+//         React.createElement(
+//           "li",
+//           {},
+//           React.createElement("a", { href: "#" }, "hello") // a tag'ine href eklenmesi
+//         ),
+//         React.createElement(
+//           "li",
+//           {},
+//           React.createElement("a", { href: "#" }, "world") // a tag'ine href eklenmesi
+//         ),
+//         React.createElement(
+//           "li",
+//           {},
+//           React.createElement("a", { href: "#" }, "from") // a tag'ine href eklenmesi
+//         ),
+//         React.createElement(
+//           "li",
+//           {},
+//           React.createElement("a", { href: "#" }, "react") // a tag'ine href eklenmesi
+//         ),
+//         React.createElement(
+//           "li",
+//           {},
+//           React.createElement("a", { href: "#" }, "universe") // a tag'ine href eklenmesi
+//         )
+//       )}
+//     </React.Fragment>
+//   );
+// };
+
 const Greeting = () => {
   return (
     <React.Fragment>
-      {React.createElement("h2", {}, "Hello from react")}
-      {React.createElement(
-        "ul",
-        {}, // ul elementinin özellikleri (props) burada boş
-        React.createElement(
-          "li",
-          {},
-          React.createElement("a", { href: "#" }, "hello") // a tag'ine href eklenmesi
-        ),
-        React.createElement(
-          "li",
-          {},
-          React.createElement("a", { href: "#" }, "world") // a tag'ine href eklenmesi
-        ),
-        React.createElement(
-          "li",
-          {},
-          React.createElement("a", { href: "#" }, "from") // a tag'ine href eklenmesi
-        ),
-        React.createElement(
-          "li",
-          {},
-          React.createElement("a", { href: "#" }, "react") // a tag'ine href eklenmesi
-        ),
-        React.createElement(
-          "li",
-          {},
-          React.createElement("a", { href: "#" }, "universe") // a tag'ine href eklenmesi
-        )
-      )}
+      <Person name="Ahmed Yusuf" />
+      <Hello />
     </React.Fragment>
   );
 };
 
+const Person = (props) => {
+  return (
+    <React.Fragment>
+      {React.createElement("h2", {}, `My name is ${props.name}`)}
+    </React.Fragment>
+  );
+};
+
+const Hello = () => {
+  return <>{React.createElement("p", {}, "This is react studying")}</>;
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<Greeting />);
