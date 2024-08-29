f1d0357222207ef6712aefcc643e9c5048fee07d
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-08-29 10:48:25 +0300

    Up

--- firstproject/src/index.js
+++ firstproject/src/index.js
@@ -1,8 +1,20 @@
-import React, { startTransition } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 
+// function Greeting() {
+//   return <h2>This is first component</h2>;
+// }
 function Greeting() {
-  return <h2>This is first component</h2>;
+  return (
+    <>
+      {React.createElement(
+        "div",
+        { className: "my-container" },
+        React.createElement("div", { className: "second-heading" }, "Hello"),
+        React.createElement("div", { className: "second-heading" }, "World")
+      )}
+    </>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
